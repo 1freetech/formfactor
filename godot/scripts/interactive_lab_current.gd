e94020a9bf3a42f9f9c8adaaf373cde7d8916d6e
1freetech <matthanburton@gmail.com>
2026-09-18 14:53:58 -0700

    Godot: add stable current frontend entrypoint

---
+++ godot/scripts/interactive_lab_current.gd
@@ -0,0 +1,4 @@
+extends "res://scripts/interactive_lab_1127.gd"
+
+# Stable scene entrypoint. Numbered implementation scripts remain available for
+# regression history while main.tscn can stay pointed at this file.
